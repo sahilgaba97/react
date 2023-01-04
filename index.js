36cce73afecd0d48211f1e42c9c4c61c86bbd7ee
Sahil Gaba <sahilgaba@192.168.1.4>
2023-01-04 18:15:23 +0530

    create element using react.createElement

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
-const heading = document.createElement("h1");
+// const heading = document.createElement("h1");
 
-heading.innerHTML = "Hello World!";
+// heading.innerHTML = "Hello World!";
 
-const root = document.getElementById("root");
+// const root = document.getElementById("root");
 
-root.appendChild(heading);
+// root.appendChild(heading);
+
+//console.log("working...");
+
+const heading1 = React.createElement(
+  "h1",
+  {
+    id: "heading1",
+  },
+  "Heading1"
+);
+
+const heading2 = React.createElement(
+  "h1",
+  {
+    id: "heading2",
+  },
+  "Heading2"
+);
+
+const container = React.createElement(
+  "div",
+  {
+    id: "container",
+  },
+  heading1,
+  heading2
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container);
